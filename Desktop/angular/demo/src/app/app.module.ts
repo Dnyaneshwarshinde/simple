46f74e2c13ee2e86f1192e33c37c4d5c3e4a2c1d
D S <dnyaneshwarshinde003@gmail.com>
2019-11-15 16:01:49 +0530

    angular added

--- Desktop/angular/demo/src/app/app.module.ts
+++ Desktop/angular/demo/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmpComponent } from './emp/emp.component';
+import { DeptComponent } from './dept/dept.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpComponent,
+    DeptComponent
   ],
   imports: [
     BrowserModule
